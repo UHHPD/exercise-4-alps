a364fa87424b4eb886fdfff61c1fc61d5c80e85e
tiacrane <96049871+tiacrane@users.noreply.github.com>
2023-12-10 23:32:07 +0100

    Update run.cc

--- run.cc
+++ run.cc
@@ -2,6 +2,8 @@
 #include <vector>
 #include <functional>
 #include <string>
+#include <list>
+#include <cmath>
 
 #include "Data.hh"
 
@@ -67,13 +69,46 @@ int main() {
   runTests();
   cout << "******************************************************" << endl;
   // create an object which holds data of experiment A
-  Data datA("exp_A");
+  std::vector<Data> exps;
+  for (auto s : {"exp_A","exp_B","exp_C","exp_D"}){
+    exps.push_back(Data(s));
+  }
 
-  // here is the data from experiment A
-  cout << "bin 27: from " << datA.binLow(27) << " to " << datA.binHigh(27)
-       << endl;
-  cout << "measurement of experiment A in bin 27: " << datA.measurement(27)
-       << endl;
+std::vector<char>  exp_label = {'A','B','C','D'};
 
-  return 0;
+double error_sum = 0;
+for(int i = 0; i < exp_label.size(); ++i){
+  // here is the data from experiment A  ;
+  cout << "bin 27: from " << exps[i].binLow(27) << " to " << exps[i].binHigh(27)
+       << endl;
+  cout << "measurement of experiment " << exp_label[i] << " in bin 27: " << exps[i].measurement(27)
+       << " +/- " << exps[i].error(27)  << endl;
+  error_sum += exps[i].error(27)*exps[i].error(27);
 }
+
+cout << "error = " << 4*pow(error_sum,0.5) << endl;
+  return 0;
+
+for(int n = 2; n < 4; ++n){
+  int j = 0;
+  for(int i = 0; i < exp_label.size(); ++i){
+    std::cout << "exp " << exp_label[i] << "vs exp " << exp_label[i+1] << " differ in " << exps[i].checkCompatibility(exps[i+1], n) << "measurements by " << n << " sigma" << std::endl;
+  }
+}
+
+Data sum12  = exps[0] + exps[1];
+for(int i = 0; i < sum12.size(); ++i){
+  std::cout << exps[0].measurement(i) << " + " << exps[1].measurement(i) << " = " << sum12.measurement(i) << std::endl;
+  std::cout << exps[0].error(i) << ", " << exps[1].error(i) << ", " << sum12.error(i) << std::endl;
+} 
+
+for(auto &e : exps){
+  std::cout << "chi2: " << e.chi2() << " chi2/ndf: " << e.chi2()/52 << std::endl;
+}
+
+Data sum = exps[0]+exps[1]+exps[2]+exps[3];
+std::cout << "chi2: " << sum.chi2() << " chi2/ndf: " << sum.chi2()/52 << std::endl;
+
+return 0;
+}
+
