f55f192cdad00d1e21c6b3028a43c3561d19101e
tiacrane <96049871+tiacrane@users.noreply.github.com>
2023-12-10 23:31:41 +0100

    Update Data.hh

--- Data.hh
+++ Data.hh
@@ -10,16 +10,22 @@ class Data {
 
   unsigned int size() const { return m_data.size(); }
   double measurement(int i) const { return m_data[i]; }
-  double binCenter(int i) const { return 0; }
-  double binLow(int i) const { return 0; }
-  double binHigh(int i) const { return 0; }
-  double error(int i) const { return 0; }
+  double binCenter(int i) const { return (binHigh(i)-binLow(i))/2; }
+  double binLow(int i) const { return m_bins[i]; }
+  double binHigh(int i) const { return m_bins[i+1]; }
+  double error(int i) const { return m_errors[i]; }
+  int checkCompatibility (const Data & in, int n);
+  	
+  friend Data operator+(const Data & d1, const Data & d2);
+
+  double chi2();
 
  private:
   Data() {}  // disallow empty data set
   void assertSizes();
   std::vector<double> m_data;
   std::vector<double> m_bins;
+  std::vector<double> m_errors;
 };
 
 #endif
