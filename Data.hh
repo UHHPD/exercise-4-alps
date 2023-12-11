becee4dace8544e098695ab63abb9a634373831a
ShiroeCake <148950189+ShiroeCake@users.noreply.github.com>
2023-12-11 14:15:48 +0100

    Update Data.hh
    binCenter changed

--- Data.hh
+++ Data.hh
@@ -10,13 +10,13 @@ class Data {
 
   unsigned int size() const { return m_data.size(); }
   double measurement(int i) const { return m_data[i]; }
-  double binCenter(int i) const { return (binHigh(i)-binLow(i))/2; }
+  double binCenter(int i) const { return (binHigh(i)+binLow(i))/2; }
   double binLow(int i) const { return m_bins[i]; }
   double binHigh(int i) const { return m_bins[i+1]; }
   double error(int i) const { return m_errors[i]; }
   int checkCompatibility (const Data & in, int n);
   	
-  friend Data operator+(const Data & d1, const Data & d2);
+  friend Data operator+(const Data& d1, const Data& d2);
 
   double chi2();
 
